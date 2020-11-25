3081f6c1257955c9222448b39266b9e4d0f687dc
toshiny6 <67131839+toshiny6@users.noreply.github.com>
2020-11-25 21:59:12 +0900

    a
    a

---
+++ BattleOfBlood/Assets/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static int PlayerHp = 100;
+    public static Vector3 PlayerPos;
+    public GameObject WaterBalloon;
+    public static float PlayerSpeed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 Dir = new Vector3(Mathf.Sin(DirR / 180.0f * 3.14f), 0, Mathf.Cos(DirR / 180.0f * 3.14f));
+       
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            PlayerPos += transform.forward*PlayerSpeed ;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            PlayerPos += transform.forward * PlayerSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            PlayerPos += transform.forward * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            PlayerPos += transform.forward * PlayerSpeed;
+        }
+
+        transform.position = PlayerPos;
+        PlayerPos = gameObject.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject balloon = GameObject.Instantiate(WaterBalloon)
+                as GameObject;
+
+            balloon.transform.parent = null;
+            //balloon.gameObject.layer = LayerMask.NameToLayer("Player");
+            balloon .transform.position = transform.position;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUIStyle style;
+        Rect rect;
+        int w = Screen.width, h = Screen.height;
+        rect = new Rect(0, 0, w, h * 4 / 100);
+        style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 4 / 100;
+        style.normal.textColor = Color.black;
+
+        string text = "Player HP: " + PlayerHp + "% \n";
+        GUI.Label(rect, text, style);
+    }
+}
---
+++ BattleOfBlood/Assets/WaterBalloon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBalloon : MonoBehaviour
+{
+
+    float wtimer;
+    float etimer;
+    public GameObject water = null;
+    Vector3 water_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float wtimer = Time.time;
+        etimer = wtimer+1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+
+        wtimer += Time.deltaTime;
+        if (wtimer > etimer)
+        {
+            Destroy(gameObject, 0.0f);
+            GameObject Water = GameObject.Instantiate(water);
+            water_pos.x = transform.position.x; // 구멍의 x 위치 = 아이템 공의 x 위치
+            water_pos.z = transform.position.z; // 구멍의 z 위치 = 아이템 공의 z 위치
+            Water.transform.position = water_pos; // Water 오브젝트의 위치 저장
+            Water.transform.parent = null;  //위치 독립
+        }
+    }
+}
