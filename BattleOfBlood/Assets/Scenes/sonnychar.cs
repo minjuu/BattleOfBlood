e084419db20ec017be1c08425b2f79f40d50bbaa
minjuu <minju9809@naver.com>
2020-11-25 21:18:00 +0900

    커밋
    ㅋ

---
+++ BattleOfBlood/Assets/Scenes/sonnychar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonnychar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void MyListener(bool value)
+    {
+        if (value)
+        {
+            //do the stuff when the toggle is on
+        }
+        else
+        {
+            //do the stuff when the toggle is off
+        }
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
