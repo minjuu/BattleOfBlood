2d062dcd35c0099ea24f5843b7045e632a4b8589
minjuu <minju9809@naver.com>
2020-11-25 22:37:25 +0900

    커밋
    ㅇ

--- BattleOfBlood/Assets/Scenes/selectchar.cs
+++ BattleOfBlood/Assets/Scenes/selectchar.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class selectchar : MonoBehaviour
 {
+    Toggle m_Toggle;
     // Start is called before the first frame update
     void Start()
     {
