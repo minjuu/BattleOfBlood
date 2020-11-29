cb21a6496717b4fdac1f99e16fe94bdb058d17d0
yeonh2 <72546025+yeonh2@users.noreply.github.com>
2020-11-29 21:09:07 +0900

    힐러 트리수정
    ㅇㅇ

--- BattleOfBlood/Assets/Script/BoosterAI/Booster_AI.cs
+++ BattleOfBlood/Assets/Script/BoosterAI/Booster_AI.cs
@@ -7,7 +7,6 @@ public class Booster_AI : MonoBehaviour
     private Sequence root = new Sequence();             // root 노드 생성
     private Selector selector = new Selector();         // 자식 노드들을 실행시키는 노드 생성
     private Sequence seqMoving = new Sequence();  // Enemy를 이동시키며 공격하는 기능을 하는 sequence 생성
-    private Sequence seqDetect = new Sequence();          // Enemy를 없애는 기능을 하는 sequence 생성
     private Sequence seqDead = new Sequence();
 
     private MoveBooster moveBooster = new MoveBooster();    // MoveFollowTarget 클래스 변수를 생성
@@ -25,7 +24,6 @@ public class Booster_AI : MonoBehaviour
         m_Booster = gameObject.GetComponent<BoosterMove>();
         root.AddChild(selector);
         selector.AddChild(seqDead);         // seqDead 노드를 selector의 자식 노드로 연결
-        selector.AddChild(seqDetect);       // seqRevive 노드를 selector의 자식 노드로 연결
         selector.AddChild(seqMoving); // seqMovingAttack 노드를 selector의 자식 노드로 연결
 
         moveBooster.Booster = m_Booster;      // m_Enemy를 넣어 초기화시킴
@@ -35,10 +33,9 @@ public class Booster_AI : MonoBehaviour
         boosterIsDead.Booster = m_Booster;
 
         seqMoving.AddChild(moveBooster);    //seqMovingAttack 노드에 클래스 변수들을 자식으로 추가       
-
-        seqDetect.AddChild(boosterobstacleDetect);  //seqRevive 노드에 클래스 변수를 자식으로 추가
-        seqDetect.AddChild(boosterTeamPosDetect);
-        seqDetect.AddChild(boosterEnemyPosDetect);
+        seqMoving.AddChild(boosterobstacleDetect);
+        seqMoving.AddChild(boosterTeamPosDetect);
+        seqMoving.AddChild(boosterEnemyPosDetect);
 
         seqDead.AddChild(boosterIsDead); //seqDead 노드에 클래스 변수를 자식으로 추가
 
--- BattleOfBlood/Assets/Script/HealerAI/Healer_AI.cs
+++ BattleOfBlood/Assets/Script/HealerAI/Healer_AI.cs
@@ -7,7 +7,6 @@ public class Healer_AI : MonoBehaviour
     private Sequence root = new Sequence();             // root 노드 생성
     private Selector selector = new Selector();         // 자식 노드들을 실행시키는 노드 생성
     private Sequence seqMoving = new Sequence();  // Enemy를 이동시키며 공격하는 기능을 하는 sequence 생성
-    private Sequence seqDetect = new Sequence();          // Enemy를 없애는 기능을 하는 sequence 생성
     private Sequence seqDead = new Sequence();
 
     private MoveHealer moveHealer = new MoveHealer();    // MoveFollowTarget 클래스 변수를 생성
@@ -25,7 +24,6 @@ public class Healer_AI : MonoBehaviour
         m_Healer = gameObject.GetComponent<HealerMove>();
         root.AddChild(selector);
         selector.AddChild(seqDead);         // seqDead 노드를 selector의 자식 노드로 연결
-        selector.AddChild(seqDetect);       // seqRevive 노드를 selector의 자식 노드로 연결
         selector.AddChild(seqMoving); // seqMovingAttack 노드를 selector의 자식 노드로 연결
 
         moveHealer.Healer = m_Healer;      // m_Enemy를 넣어 초기화시킴
@@ -34,11 +32,10 @@ public class Healer_AI : MonoBehaviour
         healermyHpDetect.Healer = m_Healer;
         healerIsDead.Healer = m_Healer;
 
-        seqMoving.AddChild(moveHealer);    //seqMovingAttack 노드에 클래스 변수들을 자식으로 추가       
-
-        seqDetect.AddChild(healerobstacleDetect);  //seqRevive 노드에 클래스 변수를 자식으로 추가
-        seqDetect.AddChild(healerteamHpDetect);
-        seqDetect.AddChild(healermyHpDetect);
+        seqMoving.AddChild(moveHealer);    //seqMovingAttack 노드에 클래스 변수들을 자식으로 추가
+        seqMoving.AddChild(healerobstacleDetect);
+        seqMoving.AddChild(healerteamHpDetect);
+        seqMoving.AddChild(healermyHpDetect);
 
         seqDead.AddChild(healerIsDead); //seqDead 노드에 클래스 변수를 자식으로 추가
 
