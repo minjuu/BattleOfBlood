995fccb169771fa24b8f1e1591829130cda3d8f7
minjuu <minju9809@naver.com>
2020-12-06 03:10:17 +0900

    미로 풀 ㅂ받아조 ㅜㅜㅜ+hp띄우기
    ㅡ

--- BattleOfBlood/Assets/Script/Player.cs
+++ BattleOfBlood/Assets/Script/Player.cs
@@ -14,11 +14,14 @@ public class Player : MonoBehaviour
     public static GameObject[] characters;
     public static List<GameObject> Team_array;
     public static List<GameObject> Enemy_array;
-    public static List<int> Team_Hp;
-    public static List<int> Enemy_Hp;
+    public static List<float> Team_Hp;
+    public static List<float> Enemy_Hp;
+    public static List<float> Team_Ap;
+    public static List<float> Enemy_Ap;
     public static List<Vector3> Team_Pos;
     public static List<Vector3> Enemy_Pos;
-
+    public Rigidbody b_rb;
+    public static string str;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,8 +41,14 @@ public class Player : MonoBehaviour
         Enemy_array = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
         Team_Pos = new List<Vector3>();
         Enemy_Pos = new List<Vector3>();
-        Team_Hp = new List<int>();
-        Enemy_Hp = new List<int>();
+        Team_Ap = new List<float>();
+        Enemy_Ap = new List<float>();
+
+        for (int i = 0; i < Team_Ap.Count; i++)
+        {
+            Team_Ap.Add(5.0f); //공격력 임의로 추가
+            Enemy_Ap.Add(5.0f); //공격력 임의로 추가
+        }
 
         for (int i = 0; i < Team_array.Count; i++)
         {
@@ -51,12 +60,90 @@ public class Player : MonoBehaviour
             Enemy_Pos.Add(Enemy_array[i].transform.position); //상대 팀의 위치를 리스트에 저장
         }
 
-        int count1 = 0; //Hp가 0 인 개수 찾을 생각
-        for (int i = 0; i < Team_Hp.Count; i++)
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "SampleScene")
         {
-            if (Team_Hp[i] == 0)
-                count1++;
+            for (int i = 0; i < 2; i++)
+            {
+                if (Team_array[i].name == "Player")
+                    str += "Player HP: " + Player.PlayerHp + "% \n";
+                else if (Team_array[i].name == "Sonny")
+                    str += "Sonny HP: " + SonnyMove.SonnyHp + "% \n";
+                else if (Team_array[i].name == "Bastion")
+                    str += "Bastion HP: " + BastionMove.BastionHp + "% \n";
+                else if (Team_array[i].name == "Shooter")
+                    str += "Shooter HP: " + Shooter_Move.ShooterHp + "% \n";
+                else if (Team_array[i].name == "Healer")
+                    str += "Healer HP: " + HealerMove.HealerHp + "% \n";
+                else if (Team_array[i].name == "Booster")
+                    str += "Booster HP: " + BoosterMove.BoosterHp + "% \n";
+
+                if (Enemy_array[i].name == "Player")
+                    str += "Player HP: " + Player.PlayerHp + "% \n";
+                else if (Enemy_array[i].name == "Sonny")
+                    str += "Sonny HP: " + SonnyMove.SonnyHp + "% \n";
+                else if (Enemy_array[i].name == "Bastion")
+                    str += "Bastion HP: " + BastionMove.BastionHp + "% \n";
+                else if (Enemy_array[i].name == "Shooter")
+                    str += "Shooter HP: " + Shooter_Move.ShooterHp + "% \n";
+                else if (Enemy_array[i].name == "Healer")
+                    str += "Healer HP: " + HealerMove.HealerHp + "% \n";
+                else if (Enemy_array[i].name == "Booster")
+                    str += "Booster HP: " + BoosterMove.BoosterHp + "% \n";
+            }
         }
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (Team_array[i].name == "Player")
+                    str += "Player HP: " + Player.PlayerHp + "% \n";
+                else if (Team_array[i].name == "Bastion")
+                    str += "Sonny HP: " + SonnyMove.SonnyHp + "% \n";
+                else if (Team_array[i].name == "Bastion")
+                    str += "Bastion HP: " + BastionMove.BastionHp + "% \n";
+                else if (Team_array[i].name == "Shooter")
+                    str += "Shooter HP: " + Shooter_Move.ShooterHp + "% \n";
+                else if (Team_array[i].name == "Healer")
+                    str += "Healer HP: " + HealerMove.HealerHp + "% \n";
+                else if (Team_array[i].name == "Booster")
+                    str += "Booster HP: " + BoosterMove.BoosterHp + "% \n";
+
+                if (Enemy_array[i].name == "Player")
+                    str += "Player HP: " + Player.PlayerHp + "% \n";
+                else if (Enemy_array[i].name == "Bastion")
+                    str += "Sonny HP: " + SonnyMove.SonnyHp + "% \n";
+                else if (Enemy_array[i].name == "Bastion")
+                    str += "Bastion HP: " + BastionMove.BastionHp + "% \n";
+                else if (Enemy_array[i].name == "Shooter")
+                    str += "Shooter HP: " + Shooter_Move.ShooterHp + "% \n";
+                else if (Enemy_array[i].name == "Healer")
+                    str += "Healer HP: " + HealerMove.HealerHp + "% \n";
+                else if (Enemy_array[i].name == "Booster")
+                    str += "Booster HP: " + BoosterMove.BoosterHp + "% \n";
+            }
+        }
+
+        //int count_1 = 0;
+        //for (int i = 0; i < characters.Length; i++)
+        //{
+        //    if (characters[i].tag == "Team") //tag가 Team 일때
+        //    {
+        //        Team_arr[count_1] = characters[i]; //characters배열에 있는 순서대로 재배열하여 저장
+        //        Team_Ap[count_1] = characters_Ap[i];
+        //        count_1++;
+        //    }
+        //}
+
+        //int count_2 = 0;
+        //for (int i = 0; i < characters.Length; i++)
+        //{
+        //    if (characters[i].tag == "Enemy")
+        //    {
+        //        Enemy_arr[count_2] = characters[i];
+        //        Enemy_Ap[count_1] = characters_Ap[i];
+        //        count_2++;
+        //    }
+        //}
     }
 
     // Update is called once per frame
@@ -125,11 +212,15 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+
             GameObject balloon = GameObject.Instantiate(WaterBalloon)
-                as GameObject;
+               as GameObject;
+            b_rb = balloon.GetComponent<Rigidbody>();
+            b_rb.isKinematic = false;
             Vector3 v = transform.position;
             v.y = 0.8f;
             balloon.transform.position = v;
+
         }
     }
     void OnCollisionEnter(Collision collision)
@@ -143,20 +234,33 @@ public class Player : MonoBehaviour
     void OnGUI()
     {
         GUIStyle style;
+        GUIStyle style2;
         Rect rect;
+        Rect rect2;
         int w = Screen.width, h = Screen.height;
         rect = new Rect(0, 0, w, h * 4 / 100);
+        rect2 = new Rect(w * 50 / 100, 0, w, h * 4 / 100);
         style = new GUIStyle();
+        style2 = new GUIStyle();
         style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
+        style2.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 3 / 100;
+        style2.fontSize = h * 3 / 100;
         style.normal.textColor = Color.white;
+        style2.normal.textColor = Color.white;
+        string text;
 
-        string text = "Player HP: " + Player.PlayerHp + "% \n"
-                     + "Shooter HP: " + Shooter_Move.ShooterHp + "% \n"
-                     + "Bastion HP: " + BastionMove.BastionHp + "% \n"
-                     + "Sonny HP: " + SonnyMove.SonnyHp + "% \n"
-                     + "Healer HP: " + HealerMove.HealerHp + "% \n"
-                     + "Booster HP: " + BoosterMove.BoosterHp + "% \n";
+        text = str;
+
+        string text2 = "";
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "SampleScene")
+            text2 = "Team : " + Team_array[0].name + ", " + Team_array[1].name + "\n"
+                   + "Enemy : " + Enemy_array[0].name + ", " + Enemy_array[1].name;
+        else
+            text2 = "Team : " + Team_array[0].name + ", " + Team_array[1].name + ", " + Team_array[2].name + ", " + Team_array[3] + "\n"
+                   + "Enemy : " + Enemy_array[0].name + ", " + Enemy_array[1].name + ", " + Enemy_array[2].name + ", " + Enemy_array[3] + "\n";
+
         GUI.Label(rect, text, style);
+        GUI.Label(rect2, text2, style2);
     }
 }
