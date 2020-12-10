288f94fb97187c80c4a1a5ea3e40ff7465da2853
toshiny6 <67131839+toshiny6@users.noreply.github.com>
2020-12-10 19:26:26 +0900

    씬

---
+++ BattleOfBlood/Assets/Script/SceneButton/yes2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class yes2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void onStart()
+    {
+        SceneManager.LoadScene("SampleScene"); //스테이지1 씬 전환
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
